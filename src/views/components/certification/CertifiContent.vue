45515835449ec202bb064eae4b3009ce4c8b3ea9
hwangjeonghan <hjeonghan0@gmail.com>
2024-08-12 17:08:25 +0900

    update : all v1

--- src/views/components/certification/CertifiContent.vue
+++ src/views/components/certification/CertifiContent.vue
@@ -1,278 +1,101 @@
-
 <template>
+    <div>
+      <section class="section section-shaped my-0 overflow-hidden">
+        <div class="container pt-lg pb-300">
+          <div class="row text-center justify-content-center">
+            <div class="col-lg-10">
+              <badge pill type="primary" class="my-4">New Topic</badge>
+  
+              <h2 class="display-3 text-gray mb-4">자격증 게시판인데 여기다가 뭘쓰지?</h2>
+              <p class="lead text-gray">
+                새로 올라온 취업 정보를 보여줍니다. 부트스태랩에 있는 형태대로 쓰려고 했습니다만, 우와 폰트 짱귀여워
+                <br />
+                여기는 취업 관련 내용이 올라오는 곳이라고 소개글을 작성해야지. 그래서 뭔가 깔쌈하게 정리된 취업정보를 볼 수 있는 화면으로 만들어야지.
+              </p>
+            </div>
+          </div>
+  
+          <div class="row row-grid mt-5">
+            <div class="col-lg-4" v-for="job in jobs" :key="job.jobId">
+              <div class="card shadow border-0 mb-4">
+                <div class="card-body py-5" style="min-height: 620px;">
+                  <div class="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                    <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
+                  </div>
+                  <h5 class="text-gray mt-3">{{ job.jobInfoTitle }}</h5>
+                  <ul class="list-unstyled mt-3 mb-0">
+                    <li><strong>Company:</strong> {{ job.jobCompanyName }}</li>
+                    <li><strong>Location:</strong> {{ job.jobLocation }}</li>
+                    <li><strong>Wage Type:</strong> {{ job.jobWageType }}</li>
+                    <li><strong>Salary:</strong> {{ job.jobSalary }}</li>
+                    <li><strong>Web Info:</strong> <a :href="job.jobWebInfoUrl" target="_blank">{{ job.jobWebInfoUrl }}</a></li>
+                    <li><strong>Mobile Info:</strong> <a :href="job.jobMobileInfoUrl" target="_blank">{{ job.jobMobileInfoUrl }}</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
     
 
-    <div>
-        <section class="section section-shaped my-0 overflow-hidden">
-       
-            <div class="container pt-lg pb-300">
-                <div class="row text-center justify-content-center">
-                    <div class="col-lg-10">
+          </div>
 
-                        <br>
-                        <badge pill type="primary"> New Topic </badge>
-                        <br>
-                        
-                        <h2 class="display-3 text-Gray">자격증 게시판인데 여기다가 뭘쓰지?</h2>
-                        <p class="lead text-Gray">새로 올라온 자격증 정보를 보여줍니다. 부트스태랩에 있는 형태대로 쓰려고 했습니다만, 우와 폰트 짱귀여워
-                            <br>
-                            여기는 자격증 관련 내용이 올라오는 곳이라고 소개글을 작성해야지. 그래서 뭔가 깔쌈하게 정리된 취업정보를 볼 수 있는 화면으로 만들어야지
+          
 
-                        </p>
-                    
-                    </div> 
-                </div>
-
-                <br>
-                <br>
-                <br>
-                <br>
-               
-
-                <div class="row row-grid mt-5">
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                            <badge tag="a" type="info">상세보기</badge>
-                        
-                        </router-link>
-                      
-                    </div>
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                        </router-link>
-                      
-                    </div>
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                      </router-link>
-                      
-                    </div>
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                      </router-link>
-                      
-                    </div>
-
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                       </router-link>
-                      
-                    </div>
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                    </router-link>
-                    </div>
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certifidetail">
-                        
-                            <badge tag="a" type="info">상세보기</badge>
-                        
-                        </router-link>
-                      
-                    </div>
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                    </router-link>
-                      
-                    </div>
-
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 자격증 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                    </router-link>
-                      
-                    </div>
-
-
-
-                    <div class="col-lg-4">
-                        <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
-                        
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 내용의 요약본 또는 설명글을 따로 입력하라고 해야겠다!
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                    </router-link>
-                    </div>
-                    <div class="col-lg-4">
-                        <icon name="ni ni-atom" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">우아한 형제들에서 올라온 채용공고입니다.풀스택 개발자 뽑고요 정규직이에용
-                            깃허브링크랑 지원서는 0월 0일까지 제출하세용. 이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 
-                        </p>
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                    </router-link>
-                        
-                    </div>
-                </div>
-                <div class="row row-grid mt-5">
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 내용의 요약본 또는 설명글을 따로 입력하라고 해야겠다!
-                        </p>
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                        </router-link>
-
-                    </div>
-           
-           
-                </div>
-
-                <div class="row row-grid mt-5">
-                    <div class="col-lg-4">
-                        <icon name="ni ni-settings" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 내용의 요약본 또는 설명글을 따로 입력하라고 해야겠다!
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                        </router-link>
-                    </div>
-                    <div class="col-lg-4">
-                        <icon name="ni ni-ruler-pencil" size="lg" gradient="white" shadow round color="primary"></icon>
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 내용의 요약본 또는 설명글을 따로 입력하라고 해야겠다!
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                        </router-link>
-                    </div>
-                    <div class="col-lg-4">
-                        <icon name="ni ni-atom" size="lg" gradient="white" shadow round color="primary"></icon>
-
-                        <h5 class="text-Gray mt-3">여기는 취업 관련 정보의 제목입니다.</h5>
-                        <p class="text-Black mt-3">이거는 취업 관련 내용이의 일부가 보여지는 곳입니다. 내용의 요약본 또는 설명글을 따로 입력하라고 해야겠다!
-                        </p>
-
-                        <router-link to="/certificationDetail">
-                        
-                        <badge tag="a" type="info">상세보기</badge>
-                    
-                        </router-link>
-                    </div>
-                </div>
-            </div>
-        </section>
+        </div>
+      </section>
 
     </div>
-</template>
-
-<script>
-
-
-export default {
-  name: "home",
-  components: {
-
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        jobs: [], // Job 데이터를 저장할 배열
+      };
+    },
+    mounted() {
+      this.fetchJobs(); // 컴포넌트가 마운트되면 데이터를 가져옵니다
+    },
+    methods: {
+      async fetchJobs() {
+        try {
+          const response = await axios.get('http://localhost:8083/api/jobs');
+          this.jobs = response.data; // API로부터 받은 JobEntity 데이터를 저장합니다
+        } catch (error) {
+          console.error('Error fetching job data:', error);
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .text-gray {
+    color: #6c757d !important;
   }
-};
-
-</script>
-
-
-
+  
+  .card {
+    border-radius: 0.75rem;
+    transition: transform 0.3s ease-in-out;
+  }
+  
+  .card:hover {
+    transform: translateY(-10px);
+  }
+  
+  .icon-shape-primary {
+    background-color: #5e72e4;
+    color: white;
+  }
+  
+  .icon-shape {
+    width: 3rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  </style>
+  
--- src/views/components/main/cardcompoent.vue
+++ src/views/components/main/cardcompoent.vue
@@ -3,75 +3,25 @@
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div
         class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
+        v-for="job in jobs"
+        :key="job.jobId"
+        style="border: 1px solid black; margin: 15px; box-shadow: 5px 5px 5px;"
       >
         <div class="card h-100">
           <img
             src="../../../../public/img/brand/mang.png"
             class="card-img-top"
-            alt="..."
+            alt="Job Image"
           />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">{{ job.jobInfoTitle }}</h5>
             <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div
-        class="col"
-        style="border: 1px  solid black; margin: 15px; box-shadow: 5px 5px 5px;"
-      >
-        <div class="card h-100">
-          <img
-            src="../../../../public/img/brand/mang.png"
-            class="card-img-top"
-            alt="..."
-          />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              <strong>Company:</strong> {{ job.jobCompanyName }}<br />
+              <strong>Location:</strong> {{ job.jobLocation }}<br />
+              <strong>Wage Type:</strong> {{ job.jobWageType }}<br />
+              <strong>Salary:</strong> {{ job.jobSalary }}<br />
+              <strong>Web Info:</strong> <a :href="job.jobWebInfoUrl" target="_blank">{{ job.jobWebInfoUrl }}</a><br />
+              <strong>Mobile Info:</strong> <a :href="job.jobMobileInfoUrl" target="_blank">{{ job.jobMobileInfoUrl }}</a>
             </p>
           </div>
         </div>
@@ -81,7 +31,26 @@
 </template>
 
 <script>
-export default {};
-</script>
+import axios from 'axios';
 
-<style></style>
+export default {
+  data() {
+    return {
+      jobs: [], // Job 데이터를 저장할 배열
+    };
+  },
+  mounted() {
+    this.fetchJobs(); // 컴포넌트가 마운트되면 데이터를 가져옵니다
+  },
+  methods: {
+    async fetchJobs() {
+      try {
+        const response = await axios.get('http://localhost:8083/api/jobs');
+        this.jobs = response.data; // API로부터 받은 JobEntity 데이터를 저장합니다
+      } catch (error) {
+        console.error('Error fetching job data:', error);
+      }
+    },
+  },
+};
+</script>
