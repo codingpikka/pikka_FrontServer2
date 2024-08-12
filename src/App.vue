4c61b5a1d1d915b83c6d86f192d99a4b500511c5
wsy <wsy9088@gmail.com>
2024-08-12 17:22:47 +0900

    feat post

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,10 @@
             
         </main>
         <router-view name="footer"></router-view>
-        <button @click="handler">등록하기 </button>
     </div>
 
 </template>
 
 <script setup>
-// import { FadeTransition } from "vue2-transitions";
-
-// export default {
-//   components: {
-//     FadeTransition,
-//     methods: {
-//       handler : ()=>{
-//         console.log("dfdf")
-//       }
-//     },
-//   }
-// };
-
-
-const handler = () =>{
-  fetch("http://localhost:8083/api/hello")
-  .then(reponse => reponse.json())
-  .then(data => {
-    console.log(data);
-    
-  })
-}
 
 </script>
